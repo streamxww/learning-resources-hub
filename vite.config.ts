4926463032e9f5d55cb1782bbed8cebc0eecb871
mixiaowei <mixwei@cn.ibm.com>
2025-10-27 13:52:34 +0800

    Update resources

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/learning-resources-hub/' : '/',
+  base:  '/learning-resources-hub/',
   server: {
     port: 3000,
     host: '0.0.0.0', // 允许外部访问
